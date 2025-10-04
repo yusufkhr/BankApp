f28023bfb9c6154fe91aec5c0ecab66d3c47ec65
Yusuf Kahraman <yusufkahraman022@gmail.com>
2025-10-05 00:22:38 +0300

    Customer sınıfı tamamlandı.

--- BankApp/src/Customer.java
+++ BankApp/src/Customer.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+public class Customer extends Person {
 
-public class Customer {
+    private CustomerType customerType;
+    private String email;
+
+    public Customer() {}
+
+    public Customer(CustomerType customerType, String email, String name,
+                    String lastName, String address, String phone, String city) {
+        super(name,lastName,address,phone,city);
+        this.customerType = customerType;
+        this.email = email;
+    }
+
+    public CustomerType getCustomerType() {
+        return customerType;
+    }
+    public void setCustomerType(CustomerType customerType) {
+        this.customerType = customerType;
+    }
+    public String getEmail() {
+        return email;
+    }
 }
