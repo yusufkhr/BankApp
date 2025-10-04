ee352a966673d3b81215ca8aeaf170b8f48a6c0b
Yusuf Kahraman <yusufkahraman022@gmail.com>
2025-10-05 00:07:31 +0300

    Person sınıfı oluşturuldu.

---
+++ BankApp/src/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+    private String name;
+    private String lastName;
+
+    public Person() {}
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    @Override
+    public String toString() {
+        return "Name: " +  name + ", Last Name: " + lastName;
+    }
+}
