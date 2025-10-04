724563037da8d4449b3d9ad5a74c942cec77777c
Yusuf Kahraman <yusufkahraman022@gmail.com>
2025-10-05 01:40:35 +0300

    Main de hesaplama işlemi yapıldı.

--- BankApp/src/Main.java
+++ BankApp/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
+        Calculator sum = (a,b) -> a + b;
 
-
-
+        System.out.println("Sum: " + sum.calculate(10, 20));
     }
 }
