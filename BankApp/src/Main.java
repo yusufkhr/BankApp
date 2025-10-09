aabe168f875584c6534e6f763d9a20d3ca503325
Yusuf Kahraman <yusufkahraman022@gmail.com>
2025-10-09 20:57:55 +0300

    Değişikler tamamlandı.

--- BankApp/src/Main.java
+++ BankApp/src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         Calculator sum = (a,b) -> a + b;
-
         System.out.println("Sum: " + sum.calculate(10, 20));
+
+        System.out.println();
+
     }
 }
